ad104ff7992c47cf424ea297892e042fba64d045
ajichiatsuto <s2410004@jaist.ac.jp>
2024-05-21 15:28:55 +0900

    regression_boston

---
+++ regression_boston.py
@@ -0,0 +1,10 @@
+import mglearn as mg
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+X, y = mg.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+
+print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
